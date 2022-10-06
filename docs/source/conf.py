3d8d603a2ff394e45fa26841d96f39868dcaf590
Eddie Ruiz <32622519+Ed2uiz@users.noreply.github.com>
2022-10-05 23:35:53 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,7 +90,7 @@ suppress_warnings = [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_theme = "sphinx_rtd_theme"
-html_theme = "furo"
+html_theme = "pydata_sphinx_theme"
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #html_static_path = ["_static"]
 #html_logo = "images/GiottoLogo.png"
