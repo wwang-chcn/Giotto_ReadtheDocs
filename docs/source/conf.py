88a5ee07bf322e5f3eeb8399b3a768caac5bf25f
Eddie Ruiz <32622519+Ed2uiz@users.noreply.github.com>
2022-10-11 22:33:29 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,8 +47,7 @@ extensions = [
 'sphinx_toolbox.github',
 "sphinx.ext.autosummary",
 "sphinx.ext.todo",
-"sphinx.ext.viewcode",
-"sphinx_gallery.gen_gallery"
+"sphinx.ext.viewcode"
 #'sphinx_rtd_dark_mode'
 #'sphinx_last_updated_by_git', #fatal error on readthedocs
 #'recommonmark',
